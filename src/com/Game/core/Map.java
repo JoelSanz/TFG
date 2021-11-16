9588a68da3d20239df458a690c342b66230633b0
joels <joelsanzm@gmail.com>
2021-11-16 18:10:43 +0100

    Mouse handling complete

--- src/com/Game/core/Map.java
+++ src/com/Game/core/Map.java
@@ -5,13 +5,16 @@ public class Map {
     private int[][] map;
 
     public Map(){
+
         ReadMap reader = new ReadMap(this);
     }
 
     public void setMap(int[][] map) {
+
         this.map = map;
     }
     public int getPosition(int x, int y){
+
         return map[x][y];
     }
 }
