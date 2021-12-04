11b1085a0ba6c08c4ca2f0aed2fe36924367d4b9
joels <joelsanzm@gmail.com>
2021-12-04 17:27:06 +0100

    Drawing ranges works properly

--- src/com/Game/core/Board.java
+++ src/com/Game/core/Board.java
@@ -5,18 +5,17 @@ import com.Game.InputHandler.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.StrokeBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 //import java.nio.Buffer;
 
 public class Board extends JPanel implements Runnable {
 
     boolean tested = false;
+    boolean showRanges =  true;
     Tower tower;
     boolean activeWave;
     boolean towerClicked;
@@ -63,6 +62,7 @@ public class Board extends JPanel implements Runnable {
 
         Graphics2D g2 =  (Graphics2D) g;
         g.drawImage(background, 0, 0, null);
+
         paintGameMap(g2);
         paintEnemyPath(g2);
         paintTowerMenu(g2);
@@ -73,7 +73,11 @@ public class Board extends JPanel implements Runnable {
         paintTowerMap(g2);
 
         if(towerClicked){
+
+
+            drawRangeOnMovingTower(tower, g2);
             paintTowerClicked(g2);
+
         }
         if (activeWave) {
             paintVectoids(g2);
@@ -92,55 +96,6 @@ public class Board extends JPanel implements Runnable {
 
     }
 
-    //Function that placed towers at random no longer needed
-    /*
-    public void testTowerMap(int iterations) {
-        for (int i = 0; i <= iterations; i++){
-            boolean pass = true;
-            int upperbound = 0, xPlace = 0, yPlace = 0;
-
-            while (pass) {
-                Random rand = new Random();
-                upperbound = 22;
-                xPlace = rand.nextInt(upperbound);
-                upperbound = 18;
-                yPlace = rand.nextInt(upperbound);
-                if (map.getPosition(xPlace, yPlace) != 1 && towerMap[xPlace][yPlace] == null) {
-
-                    int id = rand.nextInt(4);
-                    switch (id){
-                        case 0:
-                            BlueTower blueTower = new BlueTower(id, 5, 5, 5, "blue");
-                            towerMap[xPlace][yPlace] = blueTower;
-                            break;
-                        case 1:
-                            RedTower redTower = new RedTower(id, 5, 5, 5, "red");
-                            towerMap[xPlace][yPlace] = redTower;
-                            break;
-                        case 2:
-                            PurpleTower purpleTowerTower = new PurpleTower(id, 5, 5, 5, "purple");
-                            towerMap[xPlace][yPlace] = purpleTowerTower;
-                            break;
-                        case 3:
-                            GreenTower gTower = new GreenTower(id, 5, 5, 5, "green");
-                            towerMap[xPlace][yPlace] = gTower;
-                            break;
-                        default:
-                            System.out.println("No se que id es este");
-                    }
-
-
-
-                    pass = false;
-                }else{
-                    System.out.println("Ya hay torre o es camino");
-                }
-            }
-
-
-        }
-    }
-*/
     public static BufferedImage scale(BufferedImage imageToScale, int dWidth, int dHeight){
         BufferedImage scaledImage = null;
         if (imageToScale !=null){
@@ -151,7 +106,9 @@ public class Board extends JPanel implements Runnable {
         }
         return scaledImage;
     }
+
     private void paintTowerMap(Graphics2D g2){
+
         for(int x = 0; x < 22; x++){
             for (int y = 0; y < 18; y++){
                 if(towerMap[x][y] != null){
@@ -164,8 +121,12 @@ public class Board extends JPanel implements Runnable {
                         default -> System.out.println("este id no existe");
                     }
 
+
                     g2.drawRect(CELL_SIZE + (x * CELL_SIZE), (CELL_SIZE) + (y* CELL_SIZE), CELL_SIZE, CELL_SIZE);
+                    if(showRanges)
+                        drawRange(towerMap[x][y], x, y, g2);
                 }
+
 
 
 
@@ -173,6 +134,7 @@ public class Board extends JPanel implements Runnable {
         }
 
     }
+
     private void paintGameMap(Graphics2D g2){
         g2.setColor(Color.lightGray);
 
@@ -185,6 +147,7 @@ public class Board extends JPanel implements Runnable {
             }
         }
     }
+
     private void paintEnemyPath(Graphics2D g2){
         BasicStroke stroke;
         stroke = new BasicStroke(3,2, 2);
@@ -267,10 +230,18 @@ public class Board extends JPanel implements Runnable {
         int yInit = CELL_SIZE * 20;
         g2.drawRect( xInit, yInit, CELL_SIZE * 23,CELL_SIZE * 6 );
 
-        //Draw Spawn vectoids button
+        //Draw "Spawn vectoids" button
         g2.drawRect(xInit + CELL_SIZE, yInit + CELL_SIZE, CELL_SIZE * 5, CELL_SIZE * 2);
         g2.setFont(new Font("Times New Roman", Font.PLAIN, 30));
         g2.drawString("Spawn Vectoids", xInit + CELL_SIZE*2, yInit + CELL_SIZE *2);
+
+        //Draw "Show ranges" button
+        g2.drawRect(xInit + CELL_SIZE * 7, yInit + CELL_SIZE, CELL_SIZE * 5, CELL_SIZE * 2);
+        g2.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+        if(showRanges)
+            g2.drawString("Hide Ranges", xInit + CELL_SIZE * 8, yInit + CELL_SIZE * 2);
+        else
+            g2.drawString("Draw Ranges", xInit + CELL_SIZE * 8, yInit + CELL_SIZE * 2);
 
 
     }
@@ -287,10 +258,6 @@ public class Board extends JPanel implements Runnable {
             g2.drawImage(vectoidImage, (p.x + 1) * CELL_SIZE + (vectoidImage.getWidth() / 2), (p.y + 1) * CELL_SIZE + (vectoidImage.getHeight() / 2), null);
 
         }
-
-
-
-
     }
 
 
@@ -340,8 +307,10 @@ public class Board extends JPanel implements Runnable {
         System.out.println("checking location");
         if(x >= (CELL_SIZE * 25) && y >= (CELL_SIZE * 21) && x<= (CELL_SIZE * 30) && y<= (CELL_SIZE * 23)){
             newWave();
+        }
 
-
+        if(x >= (CELL_SIZE * 31) && y >= (CELL_SIZE * 21) && x<= (CELL_SIZE * 36) && y<= (CELL_SIZE * 23)){
+            showRanges = !showRanges;
         }
     }
     /**
@@ -354,38 +323,38 @@ public class Board extends JPanel implements Runnable {
 
         if(x >= (CELL_SIZE * 25) && y >= (CELL_SIZE * 2) && x<= (CELL_SIZE * 26) && y<= (CELL_SIZE * 3))
         {
-            tower = new BlueTower(0, 5, 5, 5, "blue");
+            tower = new BlueTower(0, 500, 5, 5, "blue");
             towerClicked = true;
         }
         else if(x >= (CELL_SIZE * 27) && y >= (CELL_SIZE * 2) && x<= (CELL_SIZE * 28) && y<= (CELL_SIZE * 3))
         {
-            tower = new GreenTower(1, 5, 5, 5, "green");
+            tower = new GreenTower(1, 200, 5, 5, "green");
             towerClicked = true;
         }
         else if(x >= (CELL_SIZE * 29) && y >= (CELL_SIZE * 2) && x<= (CELL_SIZE * 30) && y<= (CELL_SIZE * 3))
         {
-            tower = new PurpleTower(2, 5, 5, 5, "purple");
+            tower = new PurpleTower(2, 250, 5, 5, "purple");
             towerClicked = true;
         }
         else if(x >= (CELL_SIZE * 31) && y >= (CELL_SIZE * 2) && x<= (CELL_SIZE * 32) && y<= (CELL_SIZE * 3))
         {
-            tower = new RedTower(3, 5, 5, 5, "red");
+            tower = new RedTower(3, 300, 5, 5, "red");
             towerClicked = true;
         }
 
     }
-    public void newWave(){
 
-        Wave w =  new Wave(this);
+    public void newWave() {
+
+        Wave w = new Wave(this);
         w.setSpawnPoint(getSpawnPoints());
         vectoidList = new Vectoid[2];
         w.startWave(2);
 
         activeWave = true;
 
-
-
     }
+
     public Point[] getSpawnPoints(){
         Point s;
         Point[] spawnPoints = new Point[2];
@@ -402,45 +371,17 @@ public class Board extends JPanel implements Runnable {
         }
         return spawnPoints;
     }
-    //Generate Random position function no longer needed
-/*
 
-
-    /**
-     * generates a random x, y position
-     * @return An integer array containing the position generated
-
-    private int[] generateRandomPosition(){
-        boolean pass = true;
-        int position[] = new int[2];
-        while (pass) {
-            int upperbound = 0, xPlace = 0, yPlace = 0;
-            Random rand = new Random();
-            upperbound = 22;
-            xPlace = rand.nextInt(upperbound);
-            upperbound = 18;
-            yPlace = rand.nextInt(upperbound);
-            if (map.getPosition(xPlace, yPlace) != 1 && towerMap[xPlace][yPlace] == null) {
-
-                BlueTower blueTower = new BlueTower(0, 5, 5, 5, "blue");
-                towerMap[xPlace][yPlace] = blueTower;
-                pass = false;
-
-            }
-            position[0] = xPlace;
-            position[1] = yPlace;
-
-        }
-        return position;
-
-    }
-*/
     private void paintTowerClicked(Graphics2D g2){
         Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
         switch (tower.getTowerType()) {
             case "blue" -> {
                 g2.setColor(Color.blue);
-                g2.drawRect(mouseLocation.x - (CELL_SIZE / 2), mouseLocation.y - (CELL_SIZE / 2), CELL_SIZE, CELL_SIZE);
+                int x = mouseLocation.x - (CELL_SIZE / 2);
+                int y = mouseLocation.y - (CELL_SIZE / 2);
+                g2.drawRect(x, y, CELL_SIZE, CELL_SIZE);
+                drawRange(tower, x, y, g2);
+
             }
             case "red" -> {
                 g2.setColor(Color.red);
@@ -474,6 +415,28 @@ public class Board extends JPanel implements Runnable {
             towerMap[posX-1][posY-1] = tower;
         }
 
+
+    }
+
+    public void drawRange(Tower t, int x, int y, Graphics2D g2) {
+
+        int range =  t.getRange();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(new Color(0, 0, 255, 60));
+        g2.fillOval((((x+1)*CELL_SIZE)+CELL_SIZE/2)-range/2, (((y+1)*CELL_SIZE)+CELL_SIZE/2)-range/2, range, range);
+
+
+
+    }
+
+    public void drawRangeOnMovingTower(Tower t, Graphics2D g2){
+        int x = MouseInfo.getPointerInfo().getLocation().x;
+        int y = MouseInfo.getPointerInfo().getLocation().y;
+        //Ellipse2D.Double shape = new Ellipse2D.Double(x, y, 500, 500);
+        g2.setColor(Color.blue);
+        g2.drawOval(x-(t.getRange()/2), y-(t.getRange()/2), t.getRange(), t.getRange());
+        g2.setColor(new Color(0, 0, 255, 60));
+        g2.fillOval(x-(t.getRange()/2), y-(t.getRange()/2), t.getRange(), t.getRange());
 
     }
 
